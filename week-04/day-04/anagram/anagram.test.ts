ec1dca8f4b2383badd10b22b2339f6765fdbaef0
hojpaat <judit.of.horvath@gmail.com>
2019-02-07 13:48:08 +0100

    finished anagram function testing in anagram.test.ts

--- week-04/day-04/anagram/anagram.test.ts
+++ week-04/day-04/anagram/anagram.test.ts
@@ -3,3 +3,13 @@
 import { anagram } from './anagram';
 
 let test = require('tape');
+
+test('testing anagram function', t => {
+ let actual: boolean = anagram('mert érdemes', 'érdemes mert');
+ let notTrueCase: boolean = anagram('mert érdemes', 'árdemes rt e m');
+ let expected: boolean = true;
+
+ t.ok(actual, expected, 'They are anagrams');
+ t.notOk(notTrueCase, false, 'They are not anagrams');
+ t.end();
+})
