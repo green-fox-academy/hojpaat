54693f865192fa021c48dbca7a3f9e49983dbe9c
hojpaat <judit.of.horvath@gmail.com>
2019-02-07 14:13:21 +0100

    fix anagram.ts anagram function

--- week-04/day-04/anagram/anagram.ts
+++ week-04/day-04/anagram/anagram.ts
@@ -2,12 +2,12 @@
 
 function anagram(string1: string, string2: string): boolean{
   
-  let string1Array: string [] = string1.replace(' ', '').split('');
-  let string2Array: string [] = string2.replace(' ', '').split('');
-  let truthy: boolean = false;
+  let string1Array: string [] = string1.replace(/\r\n/g, '').split('');
+  let string2Array: string [] = string2.replace(/ /g, '').split('');
+  let truthy: boolean = true;
   string1Array.forEach(string => {
-    if (string2Array.indexOf(string) >= 0){
-      truthy = true;
+    if (string2Array.indexOf(string) < 0){
+      truthy = false;
     }
   })
   return truthy;
