c94cdfec59132f023515c72e8263d8257c89e0b9
hojpaat <judit.of.horvath@gmail.com>
2019-02-07 13:50:03 +0100

    improve the anagram function anagram.ts

--- week-04/day-04/anagram/anagram.ts
+++ week-04/day-04/anagram/anagram.ts
@@ -4,10 +4,10 @@ function anagram(string1: string, string2: string): boolean{
   
   let string1Array: string [] = string1.replace(' ', '').split('');
   let string2Array: string [] = string2.replace(' ', '').split('');
-  let truthy: boolean = true;
+  let truthy: boolean = false;
   string1Array.forEach(string => {
-    if (string2Array.indexOf(string) < 0){
-      truthy = false;
+    if (string2Array.indexOf(string) >= 0){
+      truthy = true;
     }
   })
   return truthy;
