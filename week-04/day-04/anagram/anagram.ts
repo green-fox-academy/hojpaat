0c3c84fb7913a473d39f94021b5ab093a33d90af
hojpaat <judit.of.horvath@gmail.com>
2019-02-07 15:02:42 +0100

    fix anagramma function - remove . and space from input string

--- week-04/day-04/anagram/anagram.ts
+++ week-04/day-04/anagram/anagram.ts
@@ -1,9 +1,13 @@
 'use strict';
 
 function anagram(string1: string, string2: string): boolean{
-  
+  string1 = string1.replace(/\./g, '');
+  string1 = string1.replace(/ /g, '');
+  string2 = string2.replace(/\./g, '');
+  string2 = string2.replace(/ /g, '');
   let string1Array: string [] = string1.replace(/\r\n/g, '').split('');
-  let string2Array: string [] = string2.replace(/ /g, '').split('');
+
+  let string2Array: string [] = string2.replace(/\r\n/g, '').split('');
   let truthy: boolean = true;
   string1Array.forEach(string => {
     if (string2Array.indexOf(string) < 0){
