67ce53d59fec12a138d7db1cfa56c4455a05f414
hojpaat <judit.of.horvath@gmail.com>
2019-02-14 17:22:47 +0100

    \r\n is working now

--- week-03/day-02/doubled/doubled.ts
+++ week-03/day-02/doubled/doubled.ts
@@ -2,7 +2,7 @@
 
 let fs = require('fs');
 function readFile(fileName): string[]{
-  let textArray = fs.readFileSync(fileName, 'utf-8').replace(/\r\n/g, '$$').split('');
+  let textArray = fs.readFileSync(fileName, 'utf-8').replace(/[\r\n]/g, '$$').split('');
   return textArray;  
 }
 
@@ -11,36 +11,21 @@ function readFile(fileName): string[]{
 function removeDuplication(textArray: string[]): string[]{
   let notDoubleTextArray: string[] = [];
   for(let i: number = 1; i < textArray.length; i += 2){
-    if(textArray[i] === '\r\n'){
-      notDoubleTextArray.push()
-    }
     notDoubleTextArray.push(textArray[i]);
   }
   return notDoubleTextArray;
 }
 
+
+
 function arrayToText(textArray: string[], fileName: string){
   let text = textArray.join('');
-  text.replace( '$', '!!!')
+  text = text.replace( /\$/g, '\r\n')
   fs.writeFileSync(fileName, text);
 }
 
 
-// function doubled(fileName: string){
-//   let inputTextArray = readFile('text.txt');
-//   inputTextArray = inputTextArray.map(element => removeDublication(element));
-//   let textArray = inputTextArray.map(value => value.join(''));
-//   arrayToText(textArray, 'text.txt');
-// }
-
-
 let contentArray = readFile('text.txt');
-// console.log(contentArray);
 contentArray = removeDuplication(contentArray);
 arrayToText(contentArray, 'text.txt');
 
-
-// doubled('text.txt')
-
-
-// arrayToText(inputTextArray, 'text.txt')
