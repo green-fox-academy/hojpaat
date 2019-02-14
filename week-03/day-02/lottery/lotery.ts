36ef971939203ca9d9de35fafaa19cdc39477c52
hojpaat <judit.of.horvath@gmail.com>
2019-02-14 13:33:40 +0100

    lottery numbers in a multidimensional array

--- week-03/day-02/lottery/lotery.ts
+++ week-03/day-02/lottery/lotery.ts
@@ -1,1 +1,17 @@
 'use strict';
+
+let fs = require('fs');
+
+function readFile(fileName: string): string[][]{
+  let text = fs.readFileSync(fileName, 'utf-8');
+  let textArray =  text.split('\r\n');
+  return textArray.map(value => value.split(';'))
+}
+
+function getLotteryNumber(fileName: string): string[][]{
+  let lottoNumberArray: string[][] = readFile(fileName);
+  return lottoNumberArray.map(value => value.slice(-5));
+}
+
+console.log(readFile('lotteryNumbers.txt'));
+
