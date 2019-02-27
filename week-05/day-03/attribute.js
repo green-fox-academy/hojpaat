86dac191f31676b58e2ed7d833ba18783e18a7a2
hojpaat <judit.of.horvath@gmail.com>
2019-02-27 13:15:19 +0100

    finished attribute exercise replace image task

---
+++ week-05/day-03/attribute.js
@@ -0,0 +1,13 @@
+// Write the image's url to the console.
+
+// Replace the image with a picture of your favorite animal.
+
+// Make the link point to the Green Fox Academy website.
+
+// Disable the second button.
+// Replace its text with 'Don't click me!'.
+
+
+var imgUrl = document.getElementsByTagName('img')[0];
+imgUrl.src = 'https://ih1.redbubble.net/image.208490752.9756/pp,550x550.u2.jpg';
+imgUrl.setAttribute('width', '100px')
