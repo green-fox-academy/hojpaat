1c6942259dae5b7005d1ffdf05c792d9d5483a44
hojpaat <judit.of.horvath@gmail.com>
2019-02-27 12:25:21 +0100

    finished content2 exercise first part

---
+++ week-05/day-03/content2.js
@@ -0,0 +1,14 @@
+// 1) replace the list items' content with items from this list:
+// ['apple', 'banana', 'cat', 'dog']
+
+// 2) change the <ul> element's background color to 'limegreen'
+//   - use css class to change the color instead of the style property
+
+var contentList = ['apple', 'banana', 'cat', 'dog'];
+
+var list = document.querySelectorAll('ul > li');
+
+list.forEach((element, index) => {
+  element.innerText = contentList[index];
+})
+
