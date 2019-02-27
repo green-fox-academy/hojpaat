c18ce2c94cb5989181ff14bcde1d16ac208c25a9
hojpaat <judit.of.horvath@gmail.com>
2019-02-27 11:50:25 +0100

    finished exercise1

---
+++ week-05/day-03/exercise1.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function logArray(inputArray){
+for (let i = 0; i < inputArray.length; i++) {
+  console.log(inputArray[i]);
+  ;
+};
+}
+
+var king = document.getElementById('b325');
+console.log(king);
+var businessLamp = document.getElementsByClassName('big');
+console.log('businessLamp');
+
+logArray(businessLamp);
+
+var asteroids = document.getElementsByClassName('asteroid');
+var conceitedKing = [asteroids[0], asteroids[1]];
+
+console.log('conceitedKing');
+logArray(conceitedKing)
+
+var noBusiness = [];
+
+for (var i = 0; i < asteroids.length; i++) {
+  asteroids[i].innerText !== 'The Businessman' ?
+    noBusiness.push(asteroids[i]) : '';
+}
+
+console.log('noBusiness');
+logArray(noBusiness);
+
+
