a023ec623f7ca53b4205005252c1e6b6ca94e49b
hojpaat <judit.of.horvath@gmail.com>
2019-02-28 14:01:03 +0100

    finished set up the second server with ejs

---
+++ week-06/day-04/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT} port.`)
+})
