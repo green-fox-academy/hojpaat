65a7eeddf87c797df92dfe890496cf4eac22072b
hojpaat <judit.of.horvath@gmail.com>
2019-02-28 14:20:30 +0100

    finished to do webpage with ejs file with two kind of endpoints

--- week-06/day-04/index.js
+++ week-06/day-04/index.js
@@ -6,10 +6,25 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 
+let data = {
+  title: 'MyWeb',
+  main: 'This is the main site',
+  second: 'This is the second site'
+}
+
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    title: 'Myweb',
+    main: 'This is the main site'
+  });
 });
 
+app.get('/second', (req, res) => {
+  res.render('home',{
+    title: 'MyWeb',
+    main: 'This is the second site'
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT} port.`)
