ce5058682e1c091d0ae285e1c537a8d41506bbb3
hojpaat <judit.of.horvath@gmail.com>
2019-02-28 14:46:36 +0100

    finished the exercise where the page greets the person according the querry selector, if theres none it wrties guest

---
+++ week-06/day-04/greetings/home.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home', {qs: req.query});
+})
+
+app.listen(PORT, (req, res) =>{
+  console.log(`Server started at ${PORT} port`);
+  
+})
