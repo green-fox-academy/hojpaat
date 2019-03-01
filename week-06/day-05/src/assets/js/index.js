05bbdc8d24abe056905fa3239e3397333989db49
hojpaat <judit.of.horvath@gmail.com>
2019-03-01 12:00:48 +0100

    thumbnail pictures has opacity but can be seen when hoover, images alt also loaded into ejs

---
+++ week-06/day-05/src/assets/js/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+
+ imagesPath : ['assets/images/img01.jpg', 'assets/images/img02.jpg', 'assets/images/img03.jpg', 'assets/images/img04.jpg', 'assets/images/img05.jpg', 'assets/images/img06.jpg'],
+
+ imagesDescription : ['Sunset', 'Grand Canyon', 'Camp Cretaceous', 'Timna Park', 'Taknafjördur', 'Black Beach']
+
+}
--- week-06/day-05/src/server.js
+++ week-06/day-05/src/server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const moduls = require('./assets/js/index')
+
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -10,7 +13,9 @@ app.use('/assets', express.static('assets'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    photos: moduls.imagesPath,
+    description: moduls.imagesDescription})
 })
 
 
