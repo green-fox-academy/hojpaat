e7b40e977db1251796904df5889f0e7516e93ccc
hojpaat <judit.of.horvath@gmail.com>
2019-03-03 19:27:33 +0100

    created an object array containing path and description, imported from the ejs file, so when the original list will change in the index.js it will change automatically

--- week-06/day-05/src/assets/js/gallery.js
+++ week-06/day-05/src/assets/js/gallery.js
@@ -1,22 +1,9 @@
 'use strict';
 
-let images = [
-  {path: 'assets/images/img01.jpg', desc: 'Sunset'},
-  {path: 'assets/images/img02.jpg', desc: 'Grand Canyon'},
-  {path: 'assets/images/img03.jpg', desc: 'Camp Cretaceous'},
-  {path: 'assets/images/img04.jpg', desc: 'Timna Park'},
-  {path: 'assets/images/img05.jpg', desc: 'Taknafjördur'},
-  {path: 'assets/images/img06.jpg', desc: 'Black Beach'}
-]
-
-
-
 function changeIndex (currentIndex, indexchange){
   let newIndex = currentIndex + indexchange;
   return newIndex
 }
-
-
 
 let mainImageIndex = 0;
 let mainImage = document.getElementById('mainPicture');
@@ -24,20 +11,17 @@ let leftArrow = document.getElementsByClassName('left')[0];
 let rightArrow = document.getElementsByClassName('right')[0];
 let thumbnailImages = document.getElementsByClassName('smallPic');
 
+let images = [];
 
-console.log(thumbnailImages.length);
+for(let i = 0; i < thumbnailImages.length; i++){
+  images.push({path: thumbnailImages[i].getAttribute('src'), desc: thumbnailImages[i].getAttribute('alt')})
+}
 
 for(let i = 0; i < thumbnailImages.length; i++){
   thumbnailImages[i].onclick = () => {
     mainImage.setAttribute('src', thumbnailImages[i].getAttribute('src'))
   }
 }
-
-
-// thumbnailImages.forEach((pic, index) => {
-//     console.log(pic);
-
-// })
 
 rightArrow.onclick = () => {
   mainImageIndex === images.length - 1 ? mainImageIndex = 0 : mainImageIndex = changeIndex(mainImageIndex, 1);
@@ -48,4 +32,3 @@ leftArrow.onclick = () => {
   mainImageIndex === 0 ? mainImageIndex = images.length - 1 : mainImageIndex = changeIndex(mainImageIndex, -1);
   mainImage.setAttribute('src', images[mainImageIndex].path);
 }
-
