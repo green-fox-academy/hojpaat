33514fa0afa4eaaa7e6827d553ffb2b4acd73a93
hojpaat <judit.of.horvath@gmail.com>
2019-03-04 19:43:26 +0100

    create the server

--- week-07/day-02/frontend/app.js
+++ week-07/day-02/frontend/app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use('/assets', express.static('assets'))
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(PORT, () => {
+  console.log('Server started');
+  
+})
