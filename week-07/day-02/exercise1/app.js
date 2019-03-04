8cb9d73898636acfc245c106816221e67ef78ed9
hojpaat <judit.of.horvath@gmail.com>
2019-03-04 19:46:36 +0100

    restructured the folders

--- week-07/day-02/exercise1/app.js
+++ week-07/day-02/exercise1/app.js
@@ -3,11 +3,12 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 
 app.use('/assets', express.static('assets'))
 
 app.get('/', (req, res) => {
-  res.send('OK');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
