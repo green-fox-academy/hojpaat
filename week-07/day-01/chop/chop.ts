f752e4cbd2b92cc035c4a0a59f4396a99145e408
hojpaat <judit.of.horvath@gmail.com>
2019-03-04 16:17:49 +0100

    fixed when function gets an empty array

--- week-07/day-01/chop/chop.ts
+++ week-07/day-01/chop/chop.ts
@@ -3,27 +3,32 @@
 export { numberIndex };
 
 function numberIndex(inputNumber: number, inputArray: number[]): number{
-  let index: number = 0;
-  let arrayLength: number = undefined;
-  let temporaryArray: number[] = [];
-  inputArray.length % 2 === 0 ? arrayLength = inputArray.length / 2 : arrayLength = (inputArray.length - 1) / 2;
-
-  if(inputNumber <= inputArray[arrayLength - 1]){
-    temporaryArray = inputArray.slice(0, arrayLength)
-    console.log(temporaryArray, index);
+  if(inputArray[0] === undefined){
+    return -1;
   }else{
-    temporaryArray = inputArray.slice(arrayLength);
-    console.log(temporaryArray, index)
-    return index += arrayLength;
+    let index: number = 0;
+
+    let arrayLength: number = undefined;
+    let temporaryArray: number[] = [];
+    inputArray.length % 2 === 0 ? arrayLength = inputArray.length / 2 : arrayLength = (inputArray.length - 1) / 2;
+  
+    if(inputNumber <= inputArray[arrayLength - 1]){
+      temporaryArray = inputArray.slice(0, arrayLength)
+      console.log(temporaryArray, index);
+    }else{
+      temporaryArray = inputArray.slice(arrayLength);
+      console.log(temporaryArray, index)
+      return index += arrayLength;
+    }
+  
+    if(inputArray.length === 1){
+      inputArray[0] === inputNumber ? index : index = -1;
+    }else{
+      return index = numberIndex(inputNumber, temporaryArray);
+    }
+    return index
   }
 
-  if(inputArray.length === 1){
-    inputArray[0] === inputNumber ? index : index = -1;
-  }else{
-    return index = numberIndex(inputNumber, temporaryArray);
-  }
-
-  return index
 }
 
 console.log(numberIndex(23, [1, 12, 23, 44]));
