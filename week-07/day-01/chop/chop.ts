7cbf1353c22e786619e41bec93d629d1db331989
hojpaat <judit.of.horvath@gmail.com>
2019-03-04 16:09:25 +0100

    finished recursion

--- week-07/day-01/chop/chop.ts
+++ week-07/day-01/chop/chop.ts
@@ -4,22 +4,26 @@ export { numberIndex };
 
 function numberIndex(inputNumber: number, inputArray: number[]): number{
   let index: number = 0;
-  let arrayLength: number = null;
+  let arrayLength: number = undefined;
   let temporaryArray: number[] = [];
-  inputArray.length % 2 === 0 ? arrayLength = inputArray.length / 2 : arrayLength = (inputArray.length + 1) / 2;
+  inputArray.length % 2 === 0 ? arrayLength = inputArray.length / 2 : arrayLength = (inputArray.length - 1) / 2;
 
-  if(inputNumber >= inputArray[arrayLength]){
+  if(inputNumber <= inputArray[arrayLength - 1]){
     temporaryArray = inputArray.slice(0, arrayLength)
+    console.log(temporaryArray, index);
   }else{
     temporaryArray = inputArray.slice(arrayLength);
-    index += arrayLength;
+    console.log(temporaryArray, index)
+    return index += arrayLength;
   }
 
-  if(inputArray.length = 1){
+  if(inputArray.length === 1){
     inputArray[0] === inputNumber ? index : index = -1;
   }else{
-    numberIndex(inputNumber, temporaryArray);
+    return index = numberIndex(inputNumber, temporaryArray);
   }
 
   return index
 }
+
+console.log(numberIndex(23, [1, 12, 23, 44]));
