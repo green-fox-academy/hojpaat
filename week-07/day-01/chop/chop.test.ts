c392c70cf4ba74e7e7e79c4958b392cfef65e4c3
hojpaat <judit.of.horvath@gmail.com>
2019-03-04 16:14:24 +0100

    corrected the test file

--- week-07/day-01/chop/chop.test.ts
+++ week-07/day-01/chop/chop.test.ts
@@ -2,7 +2,7 @@
 
 import { numberIndex } from './chop'
 
-const test = require('test');
+const test = require('tape');
 
 test('Testing chop function', t => {
 
@@ -13,7 +13,7 @@ test('Testing chop function', t => {
   t.equal(numberIndex(50, [1, 8, 12, 40, 50, 87]), 4, 'test when the number is in the second half of the array and the element\'s number in the array is even')
   t.equal(numberIndex(50, [1, 8, 12, 40, 50, 87, 102]), 4, 'test when the number is in the second half of the array and the element\'s number in the array is odd')
   t.equal(numberIndex(5, [1, 5, 5, 12, 15, 17]), 1, 'test when the number occures more than ones is in the first half of the array and the element\'s number in the array is even');
-  t.notEqual(numberIndex(50, [1, 2, 8, 12, 50, 50, 75,80]), 4, 'test when the number occures more than ones is in the first half of the array and the element\'s number in the array is even, make sure it won\'t give back the second occurance');
-  
+  t.notEqual(numberIndex(50, [1, 2, 8, 12, 50, 50, 75,80]), 5, 'test when the number occures more than ones is in the first half of the array and the element\'s number in the array is even, make sure it won\'t give back the second occurance');
+
   t.end();
 })
