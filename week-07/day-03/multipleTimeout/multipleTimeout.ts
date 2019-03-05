76ce254e0b0a88207563c5a46a2e034e28181f8f
hojpaat <judit.of.horvath@gmail.com>
2019-03-05 22:52:59 +0100

    started the exercise, cannot figure out how to increase the timout intervall

---
+++ week-07/day-03/multipleTimeout/multipleTimeout.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Write a program that prints the following fruits:
+// apple -> immediately
+// pear -> after 1 seconds
+// melon -> after 3 seconds
+// grapes -> after 5 seconds
+
+let fruits: string[] = ['apple', 'pear', 'melon', 'grapes'];
+
+const test = (array: any[], index) => {
+  console.log(array[index]);
+}
+
+for(let i = 0; )
+setTimeout
