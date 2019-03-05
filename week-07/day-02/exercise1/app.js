7df83fc04701328286c730450585f9774ccded07
hojpaat <judit.of.horvath@gmail.com>
2019-03-05 14:10:09 +0100

    finished frontend set up and doubling part can give back a json with the doubled value but not able to response when there is no input value

--- week-07/day-02/exercise1/app.js
+++ week-07/day-02/exercise1/app.js
@@ -2,13 +2,25 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 const path = require('path');
 
+app.use(express.json());
 app.use('/assets', express.static('assets'))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.get('/doubling', (req, res) => {
+  let input = req.query.input;
+  res.json({
+    input: input,
+    double: input * 2
+  });
+
+   
 });
 
 app.listen(PORT, () => {
