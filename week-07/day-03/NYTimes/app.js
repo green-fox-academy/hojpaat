748aa7e04bd41644111f58acdda2d534d1a7cfba
hojpaat <judit.of.horvath@gmail.com>
2019-03-06 16:24:40 +0100

    finished the list with the article headline, skipplet and pub date, there is no link yet

---
+++ week-07/day-03/NYTimes/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+const apiURL= 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='
+const myKey = '&api-key=dKa1oyN3mDcaNcNHT7b6GGccC57GqPCG'
+const searchedExpression = 'apollo+11';
+
+let myRequest = new XMLHttpRequest();
+let articlesDiv = document.getElementById('articles');
+
+let newUl = document.createElement('ul');
+newUl.setAttribute('id', 'articlesList')
+articlesDiv.appendChild(newUl);
+
+let articleList = document.getElementById('articlesList')
+
+
+myRequest.open('GET', `${apiURL}${searchedExpression}${myKey}`);
+
+myRequest.onload = () => {
+  let myArticles = JSON.parse(myRequest.responseText).response.docs;
+  console.log(myArticles[0]);
+  
+  for(let i = 0; i < 20; i++){
+    let newArticleLi = document.createElement('li');
+    let newArticleH1 = document.createElement('h1');
+    newArticleH1.innerText = myArticles[i].headline.print_headline;
+    let newArticleP = document.createElement('p');
+    newArticleP.innerText = myArticles[i].headline.main;
+    let newArticleDate = document.createElement('p');
+    newArticleDate.innerText = myArticles[i].pub_date.substring(0, 10);
+
+    newArticleLi.appendChild(newArticleH1);
+    newArticleLi.appendChild(newArticleP);
+    newArticleLi.appendChild(newArticleDate);
+    articleList.appendChild(newArticleLi);
+
+  }
+  
+  
+}
+
+myRequest.send()
