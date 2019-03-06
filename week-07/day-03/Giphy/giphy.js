494127c51c4973c1e4c810842fa7c34e3b310be2
hojpaat <judit.of.horvath@gmail.com>
2019-03-06 15:29:47 +0100

    changed the searchedExpression for hug

--- week-07/day-03/Giphy/giphy.js
+++ week-07/day-03/Giphy/giphy.js
@@ -1,12 +1,7 @@
 'use strict';
 
-/*
-?q=funny+cat&api_key=YOUR_API_KEY
-api key 
-http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=AFEPytBGSvTRnhdqk3XZZTaCcINoFiWI
-*/
 const giphyEndpoint = 'http://api.giphy.com/v1/gifs/search?q='
-const searchedExpression = 'sloth'
+const searchedExpression = 'hug'
 const myKey = '&api_key=AFEPytBGSvTRnhdqk3XZZTaCcINoFiWI';
 
 
@@ -34,7 +29,6 @@ giphyRequest.onload = function () {
 }
 
 let images = document.getElementsByTagName('img');
-
 
 giphyRequest.send();
 
