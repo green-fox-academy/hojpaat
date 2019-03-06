5291e0e825ee76592ce1df128129bb8200383d81
hojpaat <judit.of.horvath@gmail.com>
2019-03-06 12:22:57 +0100

    able to add prime and nonPrime class

--- week-07/day-03/prime-squares/index.js
+++ week-07/day-03/prime-squares/index.js
@@ -2,21 +2,39 @@
 
 let section = document.getElementsByTagName('section')[0];
 
+let primeNumbers = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 
 console.log(section)
 
-function createDivs(parentElement){
-  for(let i = 0; i < 3; i++){
+function createDivs(parentElement, divNumber){
+  for(let i = 0; i < divNumber; i++){
     let newDiv = document.createElement('div');
     newDiv.innerText = i + 2;
     parentElement.appendChild(newDiv);
   }
 }
 
-createDivs(section);
+createDivs(section, 99);
 
-console.log(section);
+let divs = document.getElementsByTagName('div');
+
+function classPrime(inputElement){
+  if (primeNumbers.indexOf(parseInt(inputElement.innerText)) >= 0){
+    inputElement.setAttribute('class', 'prime');
+  }else{
+    inputElement.setAttribute('class', 'notPrime');
+  }
+}
+
+function addClassPrime(inputArray){
+
+  for(let i = 0; i < inputArray.length; i++){
+    classPrime(inputArray[i]);
+  }
+}
+
+addClassPrime(divs);
 
 
 
