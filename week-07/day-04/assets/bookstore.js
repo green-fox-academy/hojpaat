2981cd14af76bef4a70d2c2dc48f4c7e90861e2e
hojpaat <judit.of.horvath@gmail.com>
2019-03-07 13:13:58 +0100

    make an eventÃListener to list book list clicking on books button, reclick no repetition but rewritin

--- week-07/day-04/assets/bookstore.js
+++ week-07/day-04/assets/bookstore.js
@@ -6,26 +6,31 @@ const ssearchedExpression = '';
 
 const myRequest = new XMLHttpRequest();
 
-myRequest.open('GET', booksEndpoint);
+const booksButton = document.getElementById('books');
+let h1 = document.getElementsByTagName('h1')[0];
+const main = document.getElementById('main');
 
-myRequest.onload = () => {
-  let books = JSON.parse(myRequest.responseText);
-  let h1 = document.getElementsByTagName('h1');
-  h1.innerText = 'My books';
-
-  let main = document.getElementById('main');
-  let newUl = document.createElement('ul');
-  newUl.setAttribute('id', 'bookList')
-
-  for(let i = 0; i < books.length; i++){
-    let newLi = document.createElement('li');
-    newLi.setAttribute('class', 'book');
-    newLi.innerText = books[i].book_name;
-
-    newUl.appendChild(newLi);    
+let newUl = document.createElement('ul');
+booksButton.addEventListener('click', () => {
+  myRequest.open('GET', booksEndpoint);
+  
+  myRequest.onload = () => {
+    newUl.innerText = '';
+    let books = JSON.parse(myRequest.responseText);
+    h1.innerText = 'Our books';
+  
+    newUl.setAttribute('id', 'bookList')
+  
+    for(let i = 0; i < books.length; i++){
+      let newLi = document.createElement('li');
+      newLi.setAttribute('class', 'book');
+      newLi.innerText = books[i].book_name;
+  
+      newUl.appendChild(newLi);    
+    }
+    main.appendChild(newUl);
   }
-  main.appendChild(newUl);
-}
-
-
-myRequest.send()
+  
+  
+  myRequest.send()
+})
