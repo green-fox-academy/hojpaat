ca6ada2851fca232aac18a67c4bd6dd80a4ef14e
hojpaat <judit.of.horvath@gmail.com>
2019-03-07 14:32:38 +0100

    it use remove to delete the main div content when its a cick on the books button, so wont be repetition

--- week-07/day-04/assets/bookstore.js
+++ week-07/day-04/assets/bookstore.js
@@ -3,7 +3,7 @@
 
 const booksEndpoint = '/books' //because cross referencing localhost:3000/books is not good
 const detailsEndpoint = '/details';
-
+let mainUl = ''
 const myRequest = new XMLHttpRequest();
 
 let tableHeader = ['title', 'authors', 'catogry', 'publisher', 'price'];
@@ -12,59 +12,65 @@ const deatailsButton = document.getElementById('details');
 let h1 = document.getElementsByTagName('h1')[0];
 const main = document.getElementById('main');
 
-let newUl = document.createElement('ul');
+
+
 booksButton.addEventListener('click', () => {
+  if(typeof main.children[0] !== 'undefined'){
+    main.children[0].remove();
+  }
   myRequest.open('GET', booksEndpoint);
   
   myRequest.onload = () => {
-    newUl.innerText = '';
+    let newUl = document.createElement('ul');
+    newUl.setAttribute('id', 'bookList')
+
     let books = JSON.parse(myRequest.responseText);
     h1.innerText = 'Our books';
-    
-    newUl.setAttribute('id', 'bookList')
-    
-    for(let i = 0; i < books.length; i++){
+
+
+    for (let i = 0; i < books.length; i++) {
       let newLi = document.createElement('li');
       newLi.setAttribute('class', 'book');
       newLi.innerText = books[i].book_name;
       
-      newUl.appendChild(newLi);    
+      newUl.appendChild(newLi);
     }
     main.appendChild(newUl);
   }
-  
   
   myRequest.send()
 })
 
 deatailsButton.addEventListener('click', () => {
-  // myRequest.open('GET', booksEndpoint);
-  
+  myRequest.open('GET', booksEndpoint);
+
   // myRequest.onload = () => {
   //   newUl.innerText = '';
   //   let books = JSON.parse(myRequest.responseText);
   //   h1.innerText = 'Details';
-  
+
   //   newUl.setAttribute('id', 'bookList')
-  
+
   //   for(let i = 0; i < books.length; i++){
   //     let newLi = document.createElement('li');
   //     newLi.setAttribute('class', 'book');
   //     newLi.innerText = books[i].book_name;
-  
+
   //     newUl.appendChild(newLi);    
   //   }
   //   main.appendChild(newUl);
   // }
-  
-  
+
+
   // myRequest.send()
 
   let newTable = document.createElement('table');
-  for(let i = 0; i < tableHeader.length; i++){
+  for (let i = 0; i < tableHeader.length; i++) {
     let newTh = document.createElement('th');
     newTh.innerText = tableHeader[i];
     newTable.appendChild(newTh);
   }
   main.appendChild(newTable);
+
+
 })
