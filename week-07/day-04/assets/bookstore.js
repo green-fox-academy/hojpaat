2dfcbafa4c0d6db69a4767b1f8e6aa459ae20a9d
hojpaat <judit.of.horvath@gmail.com>
2019-03-07 13:44:54 +0100

    details button creates a table with a header

--- week-07/day-04/assets/bookstore.js
+++ week-07/day-04/assets/bookstore.js
@@ -6,6 +6,7 @@ const detailsEndpoint = '/details';
 
 const myRequest = new XMLHttpRequest();
 
+let tableHeader = ['title', 'authors', 'catogry', 'publisher', 'price'];
 const booksButton = document.getElementById('books');
 const deatailsButton = document.getElementById('details');
 let h1 = document.getElementsByTagName('h1')[0];
@@ -19,14 +20,14 @@ booksButton.addEventListener('click', () => {
     newUl.innerText = '';
     let books = JSON.parse(myRequest.responseText);
     h1.innerText = 'Our books';
-  
+    
     newUl.setAttribute('id', 'bookList')
-  
+    
     for(let i = 0; i < books.length; i++){
       let newLi = document.createElement('li');
       newLi.setAttribute('class', 'book');
       newLi.innerText = books[i].book_name;
-  
+      
       newUl.appendChild(newLi);    
     }
     main.appendChild(newUl);
@@ -37,25 +38,33 @@ booksButton.addEventListener('click', () => {
 })
 
 deatailsButton.addEventListener('click', () => {
-  myRequest.open('GET', booksEndpoint);
+  // myRequest.open('GET', booksEndpoint);
   
-  myRequest.onload = () => {
-    newUl.innerText = '';
-    let books = JSON.parse(myRequest.responseText);
-    h1.innerText = 'Details';
+  // myRequest.onload = () => {
+  //   newUl.innerText = '';
+  //   let books = JSON.parse(myRequest.responseText);
+  //   h1.innerText = 'Details';
   
-    newUl.setAttribute('id', 'bookList')
+  //   newUl.setAttribute('id', 'bookList')
   
-    for(let i = 0; i < books.length; i++){
-      let newLi = document.createElement('li');
-      newLi.setAttribute('class', 'book');
-      newLi.innerText = books[i].book_name;
+  //   for(let i = 0; i < books.length; i++){
+  //     let newLi = document.createElement('li');
+  //     newLi.setAttribute('class', 'book');
+  //     newLi.innerText = books[i].book_name;
   
-      newUl.appendChild(newLi);    
-    }
-    main.appendChild(newUl);
+  //     newUl.appendChild(newLi);    
+  //   }
+  //   main.appendChild(newUl);
+  // }
+  
+  
+  // myRequest.send()
+
+  let newTable = document.createElement('table');
+  for(let i = 0; i < tableHeader.length; i++){
+    let newTh = document.createElement('th');
+    newTh.innerText = tableHeader[i];
+    newTable.appendChild(newTh);
   }
-  
-  
-  myRequest.send()
+  main.appendChild(newTable);
 })
--- week-07/day-04/server_bookstore.js
+++ week-07/day-04/server_bookstore.js
@@ -17,6 +17,11 @@ const conn = mysql.createConnection({
   database: process.env.DB_DATABASE
 })
 
+app.get('/', (req, res) => {
+  
+  res.sendFile(path.join(__dirname, './bookstore.html'))
+})
+
 app.get('/books', (req, res) => {
   let SQL = 'SELECT book_name FROM book_mast;'
   conn.query(SQL, (err, rows) => {
@@ -29,10 +34,18 @@ app.get('/books', (req, res) => {
   });
 });
 
-app.get('/', (req, res) => {
-  
-  res.sendFile(path.join(__dirname, './bookstore.html'))
-})
+app.get('/details', (req, res) => {
+  let SQL = 'SELECT b.book_name, a.aut_name, cat.cate_descrip, pub.pub_name, b.book_price FROM book_mast b LEFT JOIN author a ON b.aut_id = a.aut_id LEFT JOIN category cat ON b.cate_id = cat.cate_id LEFT JOIN publisher pub ON b.pub_id = pub.pub_id;'
+  conn.query(SQL, (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send();
+      return;
+    }
+    res.send(rows);
+  });
+});
+
 
 
 
