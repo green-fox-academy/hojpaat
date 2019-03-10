4682fc55c4fd8886cb5085f10435650e606a6880
hojpaat <judit.of.horvath@gmail.com>
2019-03-10 20:53:02 +0100

    finished exercise counter

--- week-08/day-01/counter/app.js
+++ week-08/day-01/counter/app.js
@@ -1,1 +1,14 @@
 "use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+  let button = document.querySelector('button');
+  let listElement = document.querySelectorAll('li');
+  let result = document.querySelector('.result');
+
+  
+  button.addEventListener('click', () => {
+    result.textContent = `The number of the elemens is ${listElement.length}.`;
+
+  });
+  
+})
