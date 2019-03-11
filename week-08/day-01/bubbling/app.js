e43e43f411b9f626698a49e2cf54a9313198b6fe
hojpaat <judit.of.horvath@gmail.com>
2019-03-11 11:40:33 +0100

    finished bubbling exercise

--- week-08/day-01/bubbling/app.js
+++ week-08/day-01/bubbling/app.js
@@ -5,8 +5,34 @@ console.log('started');
 
 document.addEventListener('DOMContentLoaded', () => {
   let navBar = document.getElementsByTagName('nav')[0];
+  let img = document.querySelector('.img-inspector');
+  let imgSize = 200;
+  let imgPosition = {
+    x: 0,
+    y: 0
+  }
+  console.log(imgSize);
+  
   navBar.addEventListener('click', (e) => {
-    console.log(e.target);
+    let dataAction = e.target.dataset.action;
+    let dataDirection = e.target.dataset.direction;
+    if(dataAction === 'zoom'){
+      dataDirection === 'in' && imgSize > 20 ? imgSize += 20 : imgSize -= 20;
+      img.setAttribute('style', `background-size: ${imgSize}%`)
+    }else if(dataAction = 'move'){
+      
+      if(dataDirection === 'up'){
+        imgPosition.y += 10;
+      }else if(dataDirection === 'down'){
+        imgPosition.y -= 10;
+      }else if(dataDirection === 'right'){
+        imgPosition.x += 10;
+      }else if(dataDirection === 'left'){
+        imgPosition.x -= 10;
+      }
+      
+      img.setAttribute('style', `background-position: ${imgPosition.x}px ${imgPosition.y}px;`);
+    }
     
   })
 })
