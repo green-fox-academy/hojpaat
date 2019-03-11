b48935e2a2c8a961c396efac851a0cc5e328ba55
hojpaat <judit.of.horvath@gmail.com>
2019-03-11 11:41:31 +0100

    delete the exercise description

--- week-08/day-01/bubbling/app.js
+++ week-08/day-01/bubbling/app.js
@@ -36,25 +36,3 @@ document.addEventListener('DOMContentLoaded', () => {
     
   })
 })
-// document.addEventListener('DOMContentLoad', () => {
-//   console.log('started');
-  
-//   })
-// })
-
-// <!-- You can work in the html or in a separate js file. Like:
-
-//     <script>
-//       Here is an image inspector, but the buttons are not implemented yet, that will
-//       be your task!
-//        - the nav buttons (up, down, left, right) move the background by 10px
-//        - the zoom buttons increase/decrease the image by 20%
-//        - attach only two event listeners to the nav element
-//          - one for navigation
-//          - one for zooming
-//     </script>
-
-//     OR
-
-//     <script src="bubbling.js"></script> -->
-//   </body>
