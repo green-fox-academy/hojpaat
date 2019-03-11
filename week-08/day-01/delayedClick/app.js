eb405b02f6dbdfc97c1695580104048be4277be3
hojpaat <judit.of.horvath@gmail.com>
2019-03-11 12:00:05 +0100

    finished delayed click exercise

--- week-08/day-01/delayedClick/app.js
+++ week-08/day-01/delayedClick/app.js
@@ -1,8 +1,17 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  let button = document.querySelector('button');
-  button.addEventListener('click', () => {
+function message(inputMessage){
+  let body = document.querySelector('body');
+  let newP = document.createElement('div');
+  newP.innerText = inputMessage;
+  body.appendChild(newP);
+}
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    let button = document.querySelector('button');
+    
+    button.addEventListener('click', () => {
 
+      setTimeout(()=>message('2 second ellapsed'), 2000);
   })
 })
