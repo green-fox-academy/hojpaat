21a895012acc1480f7737d2fa9e4198ae6ff0272
hojpaat <judit.of.horvath@gmail.com>
2019-03-11 15:57:36 +0100

    finished random number generator

--- week-08/day-01/infiniteScroll/app.js
+++ week-08/day-01/infiniteScroll/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const randomNumber = function randomNumberGenerator (min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  console.log(randomNumber(20, 0));
+  console.log(randomNumber(20, 0));
+  console.log(randomNumber(20, 0));
+  console.log(randomNumber(20, 0));
+  console.log(randomNumber(20, 0));
+
+})
