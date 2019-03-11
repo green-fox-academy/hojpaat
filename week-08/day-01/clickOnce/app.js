df21f6a4fe9363ca74347fdf62eb5ca4b0c3e3b2
hojpaat <judit.of.horvath@gmail.com>
2019-03-11 10:59:53 +0100

    add second solution to clickOnce app

--- week-08/day-01/clickOnce/app.js
+++ week-08/day-01/clickOnce/app.js
@@ -2,9 +2,19 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   let button = document.querySelector('button');
-
-  button.addEventListener('click', () => {
+//solution1
+  /*button.addEventListener('click', () => {
     console.log(Date());
-    button.setAttribute('disabled', 'true');
+    button.setAttribute('disabled', 'true');    
+  })*/
+//solution2
+  button.addEventListener('click', (e) => {
+    let buttonClasses = Array.from(button.classList);
+  if(buttonClasses.indexOf('unclickable') < 0){
+    console.log(Date());
+  button.setAttribute('class', 'unclickable');
+  }else{
+    e.preventDefault();
+  }
   })
 })
