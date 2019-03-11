e786103cfebe8e71029502aa7a288f346290fbbf
hojpaat <judit.of.horvath@gmail.com>
2019-03-11 10:31:43 +0100

    deleted the exercise description

--- week-08/day-01/clickOnce/app.js
+++ week-08/day-01/clickOnce/app.js
@@ -8,14 +8,3 @@ document.addEventListener('DOMContentLoaded', () => {
     button.setAttribute('disabled', 'true');
   })
 })
-
-
-//   Create an event listener which logs the current timestamp to the console once the button is clicked.
-//   When the button is clicked for the second or any other times no event listener should be called.
-//   Try to solve this two different ways.
-//   hint: take a look at the button attributes https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button
-// </script>
-
-// OR
-
-// <script src="one-time-clicker.js"></script> -->
