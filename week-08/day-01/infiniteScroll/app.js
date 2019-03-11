dbb07b5dbe4a93d2c76273c166d49f1ab36f9750
hojpaat <judit.of.horvath@gmail.com>
2019-03-11 16:05:59 +0100

    able to create a given number of divs with random colors

--- week-08/day-01/infiniteScroll/app.js
+++ week-08/day-01/infiniteScroll/app.js
@@ -1,14 +1,24 @@
 'use strict';
 
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const randomNumber = function randomNumberGenerator (min, max){
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.round(Math.random() * (max - min) + min);
   }
   
-  console.log(randomNumber(20, 0));
-  console.log(randomNumber(20, 0));
-  console.log(randomNumber(20, 0));
-  console.log(randomNumber(20, 0));
-  console.log(randomNumber(20, 0));
+  const createDiv = function divCreation(numberOfDivs){
+    for( let i = 0; i < numberOfDivs; i++){
+      let newDiv = document.createElement('div');
+      newDiv.setAttribute('style', `background: rgb(${randomNumber(0, 255)}, ${randomNumber(0, 255)}, ${randomNumber(0, 255)},`)
+      body.appendChild(newDiv);
+    }
+  }
+
+  let body = document.querySelector('body');
+
+  createDiv(3);
+
 
 })
