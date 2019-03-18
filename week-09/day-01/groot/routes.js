c61bc678d6463d86d406a448d80892202382deb0
hojpaat <judit.of.horvath@gmail.com>
2019-03-18 13:34:37 +0100

    fixed the routes.js

--- week-09/day-01/groot/routes.js
+++ week-09/day-01/groot/routes.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/groot', (req, res) => {
   let message = req.query.message;
