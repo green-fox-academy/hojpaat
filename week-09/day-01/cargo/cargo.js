179573ba00af08cdcf85718ec419914ef9989122
hojpaat <judit.of.horvath@gmail.com>
2019-03-18 15:53:00 +0100

    overloaded full is tested

--- week-09/day-01/cargo/cargo.js
+++ week-09/day-01/cargo/cargo.js
@@ -17,24 +17,38 @@ app.get('/rocket', (req, res) => {
   res.json(shipCargo);
 })
 
-const addAmmo = function addAllAmmo (inputObj) {
+const addAmmo = function addAllAmmo(inputObj) {
   return inputObj.caliber25 + inputObj.caliber30 + inputObj.caliber50;
 }
 
+const checkShipStatus = function setShipStatus(inputAmmoAmount, inputShip) {
+  if (inputAmmoAmount > 100) {
+    inputShip.shipstatus = 'overloaded';
+  } else if (inputAmmoAmount === 100) {
+    inputShip.shipstatus = 'full';
+  } else if (inputAmmoAmount === 0) {
+    inputShip.shipstatus = 'empty';
+  } else {
+    inputShip.shipstatus = `${inputAmmoAmount}%`;
+  }
+}
 
+const checkReady = function readyState(inputShip) {
+  inputShip.shipstatus === 'full' ? inputShip.ready = true : inputShip.ready = false
+}
 
 app.get('/rocket/fill', (req, res) => {
   let caliber = req.query.caliber * 100;
   let ammo = parseInt(req.query.amount);
-  
+
   shipCargo[`caliber${caliber}`] = ammo;
 
   let ammoSum = addAmmo(shipCargo);
+  let ammoRatio = ammoSum / maxAmmo * 100;
 
-  shipCargo.shipstatus = `${ammoSum / maxAmmo * 100}%`
+  checkShipStatus(ammoRatio, shipCargo);
+  checkReady(shipCargo);
 
-  shipCargo.shipstatus === '100%' ? shipCargo.ready = true : shipCargo.ready = false;
-  
   let answer = {
     received: req.query.caliber,
     amount: ammo,
--- week-09/day-01/cargo/test-cargo.js
+++ week-09/day-01/cargo/test-cargo.js
@@ -20,3 +20,38 @@ test('fill cargo', (t) => {
     t.end();
   })
 })
+
+test('full cargo', (t) => {
+  request(app)
+  .get('/rocket/fill?caliber=.50&amount=12500')
+  .expect('Content-type', /json/)
+  .expect(200)
+  .expect({
+    received: '.50',
+    amount: 12500,
+    shipstatus: 'full',
+    ready: true
+  })
+  .end((err, res) => {
+    t.error(err);
+    t.end();
+  })
+})
+
+test('overloaded cargo', (t) => {
+  request(app)
+  .get('/rocket/fill?caliber=.50&amount=125000')
+  .expect('Content-type', /json/)
+  .expect(200)
+  .expect({
+    received: '.50',
+    amount: 125000,
+    shipstatus: 'overloaded',
+    ready: false
+  })
+  .end((err, res) => {
+    t.error(err);
+    t.end();
+  })
+})
+
