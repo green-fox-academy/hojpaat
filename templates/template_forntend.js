1f46671bf777f85895e4d0e3bda6b6da8650b568
hojpaat <judit.of.horvath@gmail.com>
2019-03-21 22:56:39 +0100

    add some function to the front end template

--- templates/template_forntend.js
+++ templates/template_forntend.js
@@ -12,5 +12,30 @@ myRequest.onload = () => {
 myRequest.send(); //send request
 
 //stop bubbling and default behavior
-event.stopPropagation();
-event.preventDefault();
+submintBtn.onclick = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function formPost(){
+  myRequest.open('POST', '/api/links');
+  myRequest.onload = (e) => {
+    responsMessage(e);
+  }
+  myRequest.setRequestHeader('Content-Type', 'application/json');
+  myRequest.send(JSON.stringify(formData()));
+}
+
+const status = inputEvent.target.status;
+let message = '';
+let color = 'black';
+if(status === 200){
+ const response = JSON.parse(inputEvent.target.responseText);
+ message = `Your URL  is aliased to <strong>${response[0].alias}</strong> and your secret code is <strong>${response[0].secretCode}</strong>`;
+ clearForm();
+}else if (status === 400){
+  message = `Your alias is already in use!`;
+  color = 'red';
+}
+addDiv(message, color);
+}
