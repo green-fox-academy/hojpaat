f701af16ee5387566b62b5bd6c58c25da8401370
hojpaat <judit.of.horvath@gmail.com>
2019-03-21 10:28:19 +0100

    created the database and started the server

---
+++ week-09/day-04/quiz/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const mysql = require('mysql');
+const path = require('path');
+
+app.use(express.json());
+app.use('/assets', express.static('assets'));
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+})
+
+app.post('/api/questions', (req, res) => {
+  let question = req.body.question;
+  let answers = req.body.answers;
+})
