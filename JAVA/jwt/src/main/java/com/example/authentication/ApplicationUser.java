5608525b59168090dad8e22919ff60b1d4c2885a
Judit Horvath <judit.of.horvath@gmail.com>
2019-05-06 19:04:25 +0200

    feat(ApplicationUser): create class

---
+++ JAVA/jwt/src/main/java/com/example/authentication/ApplicationUser.java
@@ -0,0 +1,24 @@
+package com.example.authentication;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class ApplicationUser {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String name;
+  private String password;
+  private String email;
+}
