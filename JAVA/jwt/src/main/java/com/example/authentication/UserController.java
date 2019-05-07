060f8123edbf75f23ec2cdf998bc553d601213c7
Judit Horvath <judit.of.horvath@gmail.com>
2019-05-07 12:25:22 +0200

    feat(authentication): add JWTAuthenticationFilter

---
+++ JAVA/jwt/src/main/java/com/example/authentication/UserController.java
@@ -0,0 +1,23 @@
+package com.example.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+  
+  private UserService userService;
+  
+  @Autowired
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+  
+  @PostMapping("/register")
+  public ResponseEntity registerUser(@RequestBody ApplicationUser user){
+    return ResponseEntity.status(200).body(userService.saveUser(user));
+  }
+}
